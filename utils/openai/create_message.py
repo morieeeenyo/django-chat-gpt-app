e5c11c497c2ddc0b19c4c60cfca5150f7d2c2d99
togomoria <i.togo.mori@globis.co.jp>
2024-02-25 11:42:01 +0900

    modify: 必須パラメータだけに絞る

--- utils/openai/create_message.py
+++ utils/openai/create_message.py
@@ -10,11 +10,6 @@ def chat_gpt(prompt):
     # APIを使ってリクエストを投げる
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        temperature=0,
-        max_tokens=300,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
         messages=[
             {"role": "user", "content": prompt},
         ],
